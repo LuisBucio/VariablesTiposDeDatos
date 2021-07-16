3be2f10a3b30b74a635550ae17c24591c09fcd73
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-15 22:42:18 -0500

    Primer commit

---
+++ VariablesTiposDeDatos/src/TiposDatos.java
@@ -0,0 +1,27 @@
+
+public class TiposDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hola Mundo");
+		
+		int numero1 = 30;
+		int numero2 = 6;
+		boolean mayor18 = true;
+		int multiplicacion = numero1 * numero2;
+		String cadena1 = "Texto 1";
+		String cadena2 = "Texto 2";
+		char letra = 'y';
+		
+		String oracion = cadena1 + cadena2;
+		
+		//boolean mayor18 = true;
+		
+		System.out.println(oracion);
+		
+
+	}
+
+}
